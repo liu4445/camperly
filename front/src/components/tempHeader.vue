610deeb2062278c2c1d75a197384b6b5a566a38e
김가빈 <3to@kakao.com>
2023-11-15 15:45:35 +0900

    로그인 작업중

---
+++ front/src/components/tempHeader.vue
@@ -0,0 +1,263 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+
+const { VITE_VUE_API_URL } = import.meta.env;
+
+const ModalLoginisOpen = ref(false);
+const ModalSignUpisOpen = ref(false);
+
+const LoginUser = ref({
+  loginId: "",
+  pw: "",
+});
+
+const SignUpUser = ref({
+  loginId: "",
+  pw: "",
+  name: "",
+  tel: "",
+});
+const loginForm = () => {
+  console.log("로그인폼들어왓다.");
+  axios({
+    method: "post",
+    url: VITE_VUE_API_URL + "user/login",
+    data: {
+      loginId: LoginUser.value.loginId,
+      pw: LoginUser.value.pw,
+    },
+  })
+    .then((res) => {
+      console.log(res);
+      console.log("성공");
+      if (res.data == "") {
+        alert("아이디와 비밀번호를 확인해 주세요.");
+      } else {
+      }
+    })
+    .catch((error) => {
+      console.log(LoginUser.value);
+      console.log(error);
+    });
+};
+
+const signupForm = () => {
+  console.log("회원가입폼들어왓다.");
+  axios({
+    method: "post",
+    url: VITE_VUE_API_URL + "user/signup",
+    data: {
+      loginId: SignUpUser.value.loginId,
+      pw: SignUpUser.value.pw,
+      name: SignUpUser.value.name,
+      tel: SignUpUser.value.tel,
+    },
+  })
+    .then((res) => {
+      console.log(res);
+      console.log("성공");
+      if (res.data == "success") {
+        alert("회원가입에 성공하였습니다.");
+      }
+      //메세지에 따라서 나오는 알림 다르게.
+    })
+    .catch((error) => {
+      console.log(SignUpUser.value);
+      console.log(error);
+    })
+    .finally(() => {
+      $("#signupModal").modal("hide");
+    });
+};
+</script>
+
+<template>
+  <nav class="navbar static-top">
+    <div class="container">
+      <a class="navbar-brand fs-2" href="/">CAMPING</a>
+      <span>
+        <a
+          class="btn btn-secondary"
+          @click="ModalLoginisOpen = true"
+          data-bs-toggle="modal"
+          data-bs-target="#login"
+          >로그인 / 회원가입</a
+        >
+      </span>
+    </div>
+  </nav>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="login"
+    tabindex="-1"
+    aria-labelledby="loginModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <form class="form-box py-2" @submit.prevent="loginForm">
+            <h1 class="h3 mb-3 fw-normal">로그인</h1>
+            <div class="form-floating">
+              <input
+                type="text"
+                class="form-control mb-3"
+                id="floatingInput"
+                name="loginId"
+                placeholder="ID"
+                v-model="LoginUser.loginId"
+              />
+              <label for="floatingInput">ID</label>
+            </div>
+            <div class="form-floating">
+              <input
+                type="password"
+                class="form-control mb-3"
+                id="floatingPassword"
+                name="pw"
+                placeholder="Password"
+                v-model="LoginUser.pw"
+              />
+              <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="form-check text-start my-3 w-75">
+              <input
+                class="form-check-input mb-3"
+                type="checkbox"
+                value="remember-me"
+                id="flexCheckDefault"
+              />
+              <label class="form-check-label" for="flexCheckDefault"> 아이디 기억하기 </label>
+            </div>
+            <button class="btn btn-primary w-75 py-2" type="submit">Sign in</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#signupModal"
+            @click="ModalSignUpisOpen = false"
+          >
+            회원가입
+          </button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div
+    class="modal fade"
+    id="signupModal"
+    tabindex="-1"
+    aria-labelledby="signupModalLabel"
+    aria-hidden="{{ ModalSignUpisOpen }}"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <form class="form-box py-2" @submit.prevent="signupForm">
+            <h1 class="h3 mb-3 fw-normal">회원가입</h1>
+            <div class="form-floating">
+              <input
+                type="text"
+                class="form-control mb-3"
+                id="floatingInput"
+                name="loginId"
+                placeholder="ID"
+                v-model="SignUpUser.loginId"
+              />
+              <label for="floatingInput">ID</label>
+            </div>
+            <div class="form-floating">
+              <input
+                type="password"
+                class="form-control mb-3"
+                id="floatingPassword"
+                name="pw"
+                placeholder="Password"
+                v-model="SignUpUser.pw"
+              />
+              <label for="floatingPassword mb-3">Password</label>
+            </div>
+            <div class="form-floating">
+              <input
+                type="text"
+                class="form-control mb-3"
+                id="floatingInput"
+                name="loginName"
+                placeholder="Name"
+                v-model="SignUpUser.name"
+              />
+              <label for="floatingInput">Name</label>
+            </div>
+            <div class="form-floating">
+              <input
+                type="number"
+                class="form-control mb-3"
+                id="floatingTel"
+                name="pw"
+                placeholder="Tel"
+                v-model="SignUpUser.tel"
+              />
+              <label class="w-75" for="floatingInput">Tel</label>
+            </div>
+
+            <button class="btn btn-primary w-75 py-2" type="submit">회원 가입</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-body {
+  text-align: center;
+}
+
+.form-check {
+  margin-left: auto;
+  margin-right: auto;
+}
+.form-control {
+  margin-left: auto;
+  margin-right: auto;
+}
+.form-floating {
+  width: 75%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.navbar {
+  min-height: 10%;
+}
+span > a {
+  margin-left: 10px;
+}
+</style>
