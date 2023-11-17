2b14cec7e45e0a02fda35944a7081c920bd108fe
Jaeyeop-Jung <jjy5349@naver.com>
2023-11-17 11:20:13 +0900

    dev 브랜치 정리

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import MainHeader from "@/components/MainHeader.vue";
-import LocaTypeSelect from "./components/LocaTypeSelect.vue";
 </script>
 
 <template>
   <MainHeader />
-  <LocaTypeSelect />
 </template>
 
 <style scoped></style>
