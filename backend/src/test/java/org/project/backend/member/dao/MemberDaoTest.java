99ae522c65456f5e0193aa3c2ff9b7d61431f10f
Jaeyeop-Jung <jjy5349@naver.com>
2023-11-20 10:54:30 +0900

    MemberDaoTest 추가

--- backend/src/test/java/org/project/backend/member/dao/MemberDaoTest.java
+++ backend/src/test/java/org/project/backend/member/dao/MemberDaoTest.java
@@ -1,8 +1,13 @@
 package org.project.backend.member.dao;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.project.backend.constant.OauthLoginType;
+import org.project.backend.member.dto.MemberDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,16 +24,17 @@ class MemberDaoTest {
 		this.memberDao = memberDao;
 	}
 
-	@Test
+	@ParameterizedTest
+	@EnumSource(value = OauthLoginType.class, names = {"KAKAO", "GOOGLE"})
 	@DisplayName("로그인 ID와 OauthLoginType을 통한 조회를 테스트한다")
-	public void 로그인_ID와_OauthLoginType을_통한_조회를_테스트한다() {
-		// given
-		while (true) {
-
-		}
-
+	public void 로그인_ID와_OauthLoginType을_통한_조회를_테스트한다(OauthLoginType oauthLoginType) throws Exception {
 		// when
+		MemberDto findMember = memberDao.findByLoginIdAndOauthLoginType(
+				"test",
+				oauthLoginType
+		);
 
 		// then
+		assertThat(findMember).isNotNull();
 	}
 }
--- backend/src/test/resources/schema.sql
+++ backend/src/test/resources/schema.sql
@@ -1,6 +1,6 @@
 create table member
 (
-    USER_ID          bigint auto_increment comment '유저 ID'
+    MEMBER_ID          bigint auto_increment comment '유저 ID'
         primary key,
     LOGIN_ID         varchar(255) not null comment '유저 로그인 아이디',
     oauth_login_type varchar(255) not null,
