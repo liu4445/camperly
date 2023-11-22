6a04c52c27f98d17009aedf46b98af8e9927f621
liu4445 <liu4445@nate.com>
2023-11-22 21:44:38 +0900

    맵 만드는중

---
+++ front/src/components/Map.vue
@@ -0,0 +1,10 @@
+<script setup>
+const importURL = "//dapi.kakao.com/v2/maps/sdk.js?appkey="
+import "//dapi.kakao.com/v2/maps/sdk.js?appkey=" + ;
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
